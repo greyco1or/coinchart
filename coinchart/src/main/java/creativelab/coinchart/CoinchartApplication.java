c605d848e77169df90bdb636bcc9c9e571202def
grey <starbux630@naver.com>
2022-12-21 15:59:08 +0900

    개발환경세

---
+++ coinchart/src/main/java/creativelab/coinchart/CoinchartApplication.java
@@ -0,0 +1,13 @@
+package creativelab.coinchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinchartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinchartApplication.class, args);
+	}
+
+}
---
+++ coinchart/src/test/java/creativelab/coinchart/CoinchartApplicationTests.java
@@ -0,0 +1,13 @@
+package creativelab.coinchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinchartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
